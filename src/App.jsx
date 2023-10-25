f39589804fd33005d437bbba22d7b3dd10e7b362
Nikita Zavorotny <83165053+NikitaZavortny@users.noreply.github.com>
2023-10-25 19:40:38 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Home from './design/awesome-templates/home-template.jsx';
+import HomeFooter from './design/awesome-components/footers/footer-home.jsx';
+import MiniFooter from './design/awesome-components/footers/footer-mini.jsx';
+import CurveTransitionTemplate from './design/awesome-templates/curve-transition-template.jsx';
+
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+
+import "./bootstrap.css";
+import "./curves.scss";
+import HomePage from './design/pages/home-page.jsx';
+import ReduxSaga from './design/pages/redux-saga.jsx';
+
+function App() {
+  return (
+    <div align="center">
+
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" />
+      <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js"></script>
+
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/redux" element={<ReduxSaga />} />
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
